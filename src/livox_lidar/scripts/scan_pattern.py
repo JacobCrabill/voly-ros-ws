ba53feb863f3447763c11f7f37cdf74f2a76c036
Baixiao Huang <baixiao@flyvoly.com>
2020-08-20 11:56:50 -0700

    update

--- src/livox_lidar/scripts/scan_pattern.py
+++ src/livox_lidar/scripts/scan_pattern.py
@@ -13,7 +13,7 @@ if __name__=='__main__':
     alpha = (1.0 + n**2) / n**3
     phase_shift = alpha * d
 
-    for t in range(0, 1):
+    for t in range(0, 200):
       theta = np.arange(0, 2 * np.pi, delta_theta)
       r = 0.5 + 0.5 * np.cos(n * theta + phase_shift * t % (2 * np.pi))
       thetas = np.append(thetas, theta)
